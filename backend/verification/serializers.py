76129b2007ae2571bc72969bf6ca0f14d7a113d2
Your Name <you@example.com>
2025-12-11 00:30:10 +0300

    edit serializers

--- backend/verification/serializers.py
+++ backend/verification/serializers.py
@@ -3,6 +3,9 @@ from rest_framework import serializers
 from .models import VerificationRecord
 
 
+class FaceVerifySerializer(serializers.Serializer):
+    face_image = serializers.ImageField(required=True)
+
 class VerificationRecordSerializer(serializers.ModelSerializer):
     class Meta:
         model = VerificationRecord
